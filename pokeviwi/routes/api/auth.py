0c2cea8fe69ed6cfc05c8f53bbcb3f56fcc2e310
Zeuxis Lo <seekstudio@gmail.com>
2016-08-11 23:32:37 +0800

    improve the error handle for request google grant auth code

--- pokeviwi/routes/api/auth.py
+++ pokeviwi/routes/api/auth.py
@@ -62,60 +62,64 @@ def login_google():
     message = ""
     player  = dict()
 
-    response = requests.post("https://accounts.google.com/o/oauth2/token", data={
-        'grant_type'    : 'authorization_code',
-        'redirect_uri'  : 'urn:ietf:wg:oauth:2.0:oob',
-        'scope'         : 'openid email https://www.googleapis.com/auth/userinfo.email',
-        'client_secret' : 'NCjF1TLi2CcY6t5mt0ZveuL7',
-        'client_id'     : '848232511240-73ri3t7plvk96pj4f85uj8otdat2alem.apps.googleusercontent.com',
-        'code'          : auth_code
-    })
-
-    data = response.json()
-
-    if 'error' in data:
-        print(data)
-
-        message = data['error_description']
+    if len(auth_code) <= 0:
+        message = "Please get the auth code first"
     else:
-        access_token  = data['id_token']
-        refresh_token = data['refresh_token']
-        expires_in    = data['expires_in']
+        response = requests.post("https://accounts.google.com/o/oauth2/token", data={
+            'grant_type'    : 'authorization_code',
+            'redirect_uri'  : 'urn:ietf:wg:oauth:2.0:oob',
+            'scope'         : 'openid email https://www.googleapis.com/auth/userinfo.email',
+            'client_secret' : 'NCjF1TLi2CcY6t5mt0ZveuL7',
+            'client_id'     : '848232511240-73ri3t7plvk96pj4f85uj8otdat2alem.apps.googleusercontent.com',
+            'code'          : auth_code
+        })
 
-        api = pgoapi.PGoApi()
-        api.set_position(0, 0, 0)
+        data = response.json()
 
-        try:
-            api.set_authentication(provider = 'google', oauth2_refresh_token="oauth2rt_{0}".format(refresh_token))
-
-            api.get_auth_provider()._access_token        = access_token
-            api.get_auth_provider()._access_token_expiry = time.time() + expires_in
-            api.get_auth_provider()._login               = True
-
-            response_dict = api.get_player()
-
-            if 'responses' not in response_dict:
-                message = "Cannot find player information"
+        if 'error' in data:
+            if 'error_description' in data:
+                message = data['error_description']
             else:
-                ok          = True
-                player_data = response_dict['responses']['GET_PLAYER']['player_data']
+                message = data['error']
+        else:
+            access_token  = data['id_token']
+            refresh_token = data['refresh_token']
+            expires_in    = data['expires_in']
 
-                currencies = dict()
-                for c in player_data['currencies']:
-                    currencies[c['name'].lower()] = c['amount'] if 'amount' in c else 0
+            api = pgoapi.PGoApi()
+            api.set_position(0, 0, 0)
 
-                player = dict(
-                    username            = player_data['username'],
-                    currencies          = currencies,
-                    max_pokemon_storage = player_data['max_pokemon_storage'],
-                    max_item_storage    = player_data['max_item_storage'],
-                    created_at          = player_data['creation_timestamp_ms'],
-                )
+            try:
+                api.set_authentication(provider = 'google', oauth2_refresh_token="oauth2rt_{0}".format(refresh_token))
 
-                session['username'] = player_data['username']
-                current_app.api_container.add(player_data['username'], api)
-        except exceptions.AuthException as e:
-            message = "Invalid account information"
+                api.get_auth_provider()._access_token        = access_token
+                api.get_auth_provider()._access_token_expiry = time.time() + expires_in
+                api.get_auth_provider()._login               = True
+
+                response_dict = api.get_player()
+
+                if 'responses' not in response_dict:
+                    message = "Cannot find player information"
+                else:
+                    ok          = True
+                    player_data = response_dict['responses']['GET_PLAYER']['player_data']
+
+                    currencies = dict()
+                    for c in player_data['currencies']:
+                        currencies[c['name'].lower()] = c['amount'] if 'amount' in c else 0
+
+                    player = dict(
+                        username            = player_data['username'],
+                        currencies          = currencies,
+                        max_pokemon_storage = player_data['max_pokemon_storage'],
+                        max_item_storage    = player_data['max_item_storage'],
+                        created_at          = player_data['creation_timestamp_ms'],
+                    )
+
+                    session['username'] = player_data['username']
+                    current_app.api_container.add(player_data['username'], api)
+            except exceptions.AuthException as e:
+                message = "Invalid account information"
 
     return jsonify(
         ok      = ok,
